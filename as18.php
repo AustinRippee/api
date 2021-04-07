dc5fb583963201f574e1daaea7b55a6e8f2b9748
Austin Rippee <austinrippee@gmail.com>
2021-04-07 15:03:39 -0400

    changed table and reorganized

--- as18.php
+++ as18.php
@@ -1,21 +1,19 @@
 <DOCTYPE html>
 <html lang="en-US">
+    <!-- Page HTML and Bootstrap for the header -->
     <head>
-        <title>As18 - Covid-19 Deaths</title>
+        <title>As18 (Covid-19 Deaths)</title>
         <meta charset="hutf-8" />
         <!-- Latest compiled and minified Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-        <!-- our custom CSS -->
-        <link rel="stylesheet" href="libs/css/custom.css" />
     </head>
     
     <body>
-       
         <div class = "container">
         <?php
         // show page header
         echo "<div class='page-header'>
-                <h1>As18 - Covid-19 Deaths</h1>
+                <h1>As18 (Covid-19 Deaths)</h1>
                 <h5 style='color:grey;'>Created by Austin Rippee</h5>
                 <a href='https://github.com/AustinRippee/api.git'>Link to the GitHub Source Code </a>
             </div>";
@@ -23,67 +21,65 @@
              
              
 <?php
-// Covid19api.com deaths data
 
+// Prints the main function that displays everything
 main();
 
 function main () {
+    // Covid19api.com deaths data
 	$apiCall = 'https://api.covid19api.com/summary';
-
-	//gather into an array all countries’ deaths data
+    // Reads the data from the website and puts it into a JSON string
 	$json_string = curl_get_contents($apiCall);
+    // Stores the JSON string into a PHP object
 	$obj = json_decode($json_string);
 	
+    // Creates the deaths array for the data to be stored
 	$death_arr = Array() ;
 
+    // Sets each country with the respective deaths
 	foreach($obj->Countries as $i){
 		$death_arr[$i->Country] = $i->TotalDeaths;
 	}
 
-	//sort array in Desecending order 
+    #-----------------------------------------------------------------------------
+
+	// Sorts the array in Desecending order 
 	arsort($death_arr);
 
-	// echo html head section
-	echo '<html>';
-	echo '<head>';
-	echo '<title>COVID-19 API by Binh Dang</title>';
-	echo '<style>';
-	echo "table, th, td {
-			border: 1px solid black;
-	  	}";
-	echo '</style>';
-	echo '</head>';
-	
-	// open html body section
-	echo '<body onload="loadDoc()">';
-	// $apiCall = 'https://api.covid19api.com/summary';
-	// $json_string = curl_get_contents($apiCall);
-	// $obj = json_decode($json_string);
+    // Cuts the array into the specified number to be shown from the array
+    $death_arr = array_slice($death_arr, 0, 10);
+    // Reads the data again
+    $deaths_str = json_encode($death_arr);
+    // Then stores it into another PHP object
+    $deaths_obj = json_decode($deaths_str);
 
-    // $arr1 = Array();
-    // $arr2 = Array();
-    // foreach($obj->Countries as $i) {
-    //     array_push($arr1, $i->Country);
-    //     array_push($arr2, $i->TotalDeaths);
-    // }
-    // array_multisort($arr2, SORT_DESC, $arr1);
-    // print_r($arr1);
+    #-----------------------------------------------------------------------------
 
-    $death_arr = array_slice($death_arr,0,10); //  top 10 highest number of deaths.
-//print_r($death_arr);
-$JSONString=json_encode($death_arr);
-$JSONObject = json_decode($JSONString);
-echo '<b>Filtered JSON Object From $death_arr</b><br>';
-	echo var_dump($JSONObject);
-	echo '<br><br>';
-	echo "<div><b>Table With PHP Array</b>";
-	echo "<table>";
+    // Creates the header
+    echo "</br><div><h4><b>JSON OBJECT</b></h4>";
+    // Spits out the data from the string object from above
+	echo var_dump($deaths_obj);
+
+	echo '<br><br><br>';
+
+    #-----------------------------------------------------------------------------
+
+    // Creates the header
+	echo "<div><h4><b>TOP 10 DEATHS PER COUNTRY</b></h4>";
+    echo "<hr>";
+    // Creates the table
+	echo "<table class='table table-hover table-responsive table-bordered'>";
         echo "<tr>";
-		  	echo "<th>PHP</th>";
-            echo "<th>Country</th>";
-            echo "<th>Total Death Cases</th>";
+            // Creates the tables headers
+		  	echo "<th>RANK</th>";
+            echo "<th>COUNTRY</th>";
+            echo "<th>TOTAL DEATH CASES</th>";
 		echo "</tr>";
-		$i=1;
+
+        // Starts the country count from 1
+		$i = 1;
+
+        // Creates the loop to add each country/deaths in their respective row
 		foreach ($death_arr as $country => $cases) {
 			echo "<tr>";
 			echo "<td>{$i}</td>";
@@ -92,13 +88,11 @@ echo '<b>Filtered JSON Object From $death_arr</b><br>';
 			echo "</tr>";
 			$i++;
 		 }
+
 	echo "</table>";
 	echo '</div>';
 }
 
-
-
-//country_usa = JSON.parse(this.responseText).Countries[170].NewDeaths;
 #-----------------------------------------------------------------------------
 // read data from a URL into a string
 function curl_get_contents($url) {
